fda3b0215170615019b6eb9d3174c75af98c69c5
Devansh <devanshbansal2021@gmail.com>
2025-11-20 23:12:37 +0530

    Initial commit

---
+++ Data/VehicleProjectContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using VehicleProject.Models;
+
+namespace VehicleProject.Data;
+
+public class VehicleProjectContext : DbContext
+{
+    public VehicleProjectContext(DbContextOptions<VehicleProjectContext> options) : base(options) {
+        
+    }
+    public DbSet<Car> Cars {get; set;}
+}
---
+++ Models/Vehicle.cs
@@ -0,0 +1,9 @@
+namespace VehicleProject.Models;
+
+public abstract class Vehicle
+{
+    public int Id {get; set;}
+    public string? Make {get; set;}
+    public string? Model {get; set;}
+    public required string VehicleType {get; set;}
+}
