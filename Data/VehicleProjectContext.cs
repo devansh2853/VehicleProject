60db3e87d95e568e6383030eada39bf5b57c09f4
Devansh <devanshbansal2021@gmail.com>
2025-11-22 23:47:56 +0530

    Added OrderDS

--- Data/VehicleProjectContext.cs
+++ Data/VehicleProjectContext.cs
@@ -9,4 +9,8 @@ public class VehicleProjectContext : DbContext
         
     }
     public DbSet<Car> Cars {get; set;}
+    public DbSet<Order> Orders {get; set;}
+    public DbSet<Customer> Customers {get; set;}
+    public DbSet<Product> Products {get; set;}
+
 }
